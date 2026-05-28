e852b7ff30b9c970738e64ef2af76d73e1fa0f9f
Santino Vargas Di Buono <santinovargasdb@gmail.com>
2026-05-28 08:48:39 -0300

    Update PostCard.tsx

--- frontend/components/PostCard.tsx
+++ frontend/components/PostCard.tsx
@@ -109,7 +109,7 @@ export default function PostCard({ post, selected, onToggle }: PostCardProps) {
         display: "-webkit-box", WebkitLineClamp: 5, WebkitBoxOrient: "vertical",
         overflow: "hidden",
       }}>
-        {post.title}
+        {post.title || post.text || post.content || "Sin contenido"}
       </p>
 
       {/* Matched terms */}
